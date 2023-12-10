dcf8d6f33a09415aaa01ec06769678be20acd183
devjean <jeancarloleall@gmail.com>
2023-12-10 17:38:52 -0300

    last-step

--- src/pages/filme/index.js
+++ src/pages/filme/index.js
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import "./filme-info.css";
 import api from "../../services/api";
+import { toast } from "react-toastify";
 
 function Filme() {
   const { id } = useParams();
+  const navigate = useNavigate();
+
   const [filme, setFilme] = useState({});
   const [loading, setLoading] = useState(true);
 
@@ -19,10 +22,13 @@ function Filme() {
         })
         .then((response) => {
           setFilme(response.data);
+          // console.log(response.data);
           setLoading(false);
         })
         .catch(() => {
-          console.log("Erro ao carregar detalhes do filme");
+          console.log("FILME NAO ENCONTRADO");
+          navigate("/", { replace: true });
+          return;
         });
     }
 
@@ -31,7 +37,25 @@ function Filme() {
     return () => {
       console.log("COMPONENTE FOI DESMONTADO");
     };
-  }, []);
+  }, [navigate, id]);
+
+  function salvarFilme() {
+    const minhaLista = localStorage.getItem("@primeflix");
+
+    let filmesSalvos = JSON.parse(minhaLista) || [];
+
+    const hasFilme = filmesSalvos.some(
+      (filmeSalvo) => filmeSalvo.id === filme.id
+    );
+
+    if (hasFilme) {
+      toast.warn("Esse filme já está na sua lista!");
+      return;
+    }
+    filmesSalvos.push(filme);
+    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+    toast.success("Filme salvo com succeso!");
+  }
 
   if (loading) {
     return (
@@ -53,9 +77,15 @@ function Filme() {
       <strong>Avaliação: {filme.vote_average} / 10</strong>
 
       <div className="area-buttons">
-        <button>Salvar</button>
+        <button onClick={salvarFilme}>Salvar</button>
         <button>
-          <a href="#">Trailer</a>
+          <a
+            target="_blank"
+            rel="noopener"
+            href={`https://www.youtube.com/results?search_query=${filme.title} trailer`}
+          >
+            Trailer
+          </a>
         </button>
       </div>
     </div>
--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Filme from "./pages/filme";
+import Favoritos from "./pages/Favoritos";
 
 import Error from "./pages/Error";
+
 import Header from "./components/Header";
 
 function RoutesApp() {
@@ -14,6 +16,7 @@ function RoutesApp() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/filme/:id" element={<Filme />} />
+        <Route path="/favoritos" element={<Favoritos />} />
 
         <Route path="*" element={<Error />} />
       </Routes>
